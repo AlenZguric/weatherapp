f8b7984282edd762ec4da2da5fd50518ddeec270
Alen Zgurić <zguric.alen@hotmail.com>
2024-04-03 12:35:45 +0200

    add navbar style, pages, router dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/HomePage";
+import MyPlaces from "./pages/MyPlaces";
+import Favorites from "./pages/Favorites";
+import About from "./pages/About";
 import NavBar from "./components/header/NavBar";
-
 
 function App() {
   return (
-    <div className="App">
-      <footer>
-        <NavBar />
-      </footer>
-    </div>
+    <Router>
+      <div className="App">
+        <footer>
+          <NavBar />
+        </footer>
+        <main>
+          <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route path="/myplaces" element={<MyPlaces/>} />
+            <Route path="/favorites" element={<Favorites/>} />
+            <Route path="/about" element={<About/>} />
+          </Routes>
+        </main>
+        <footer></footer>
+      </div>
+    </Router>
   );
 }
 
--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo_dall_e3 from "../../assets/images/background_img/logo_dall_e3.jpg";
+
+import "../../assets/style/header/NavBar.css";
 
 function NavBar() {
   return (
-    <div>NavBar</div>
-  )
+    <div className="navBar">
+      <div className="group">
+        <div className="logo">
+          <Link to="/">
+            <div className="logo_img">
+              <img src={logo_dall_e3} alt="logo" />
+            </div>
+          </Link>
+        </div>
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <button>Search</button>
+        </div>
+        <div className="navigations">
+          <Link to="/">Home</Link>
+          <Link to="/myplaces">My Places</Link>
+          <Link to="/favorites">Favorites</Link>
+          <Link to="/about">About</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
