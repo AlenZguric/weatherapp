feba5df01c8deb6f1a06e0fed3bbf2c402196ba8
Alen Zgurić <zguric.alen@hotmail.com>
2024-04-03 22:49:53 +0200

    add main data,

---
+++ src/components/main/CityDetails.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import { API_Key } from "../../Api/ApiKey";
+import axios from 'axios';
+import { useParams } from "react-router-dom";
+
+const CityDetails = () => {
+  const [cityWeather, setCityWeather] = useState(null);
+  const { cityName } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_Key}`);
+        setCityWeather(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [cityName]);
+
+  if (!cityWeather) {
+    return <div>Loading...</div>;
+  }
+
+  // Prikaz detaljnih informacija o vremenu za odabrani grad
+  return (
+    <div className='city-details'>
+      <h2>{cityWeather.name} Weather Details</h2>
+      <p>Feels Like: {cityWeather.main && cityWeather.main.feels_like} °C</p>
+      <p>Temperature: {cityWeather.main && cityWeather.main.temp} °C</p>
+      <p>Min. Temp: {cityWeather.main && cityWeather.main.temp_min} °C</p>
+      <p>Max. Temp: {cityWeather.main && cityWeather.main.temp_max} °C</p>
+      <p>Wind: {cityWeather.wind && cityWeather.wind.speed} m/s</p>
+      <p>Precipitation: {cityWeather.weather && cityWeather.weather[0].main}</p>
+      <img src={`http://openweathermap.org/img/wn/${cityWeather.weather && cityWeather.weather[0].icon}.png`} alt='Weather Icon' />
+    </div>
+  );
+};
+
+export default CityDetails;
