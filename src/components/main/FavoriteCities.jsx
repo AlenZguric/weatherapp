53c0c3872f6a234ebd7aa3b65137da582515ecc3
Alen Zgurić <zguric.alen@hotmail.com>
2024-04-08 12:32:30 +0200

    add function to try, catch api and show, remove favorites from localstorage

---
+++ src/components/main/FavoriteCities.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import { fetchWeatherDataForCity } from "../../utils/fetchWeatherDataForCity ";
+import { RiDeleteBin6Line } from 'react-icons/ri';
+
+
+
+const FavoriteCities = () => {
+  const [favoriteCities, setFavoriteCities] = useState([]);
+
+  useEffect(() => {
+    const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    const fetchFavoriteCitiesWeatherData = async () => {
+      const weatherDataPromises = favorites.map(async (cityName) => {
+        const weatherData = await fetchWeatherDataForCity(cityName);
+        return weatherData;
+      });  
+
+      const favoriteCitiesWeatherData = await Promise.all(weatherDataPromises);
+      setFavoriteCities(favoriteCitiesWeatherData);
+    };
+
+    fetchFavoriteCitiesWeatherData();
+
+  }, []);
+
+  const removeFromFavorites = (cityName) => {
+    const updatedFavorites = favoriteCities.filter(city => city.name !== cityName);
+    setFavoriteCities(updatedFavorites);
+    localStorage.setItem("favorites", JSON.stringify(updatedFavorites.map(city => city.name)));
+  };
+
+
+  return (
+    <div>
+      <h2>Favorite Cities</h2>
+      <ul>
+        {favoriteCities.map((cityWeather, index) => (
+          <li key={index}>
+            {cityWeather ? (
+              <div>
+                <h3>{cityWeather.name}</h3>
+                <p>Temperature: {cityWeather.main.temp} °C</p>
+                <p>feels like: {cityWeather.main.feels_like}</p>
+                <RiDeleteBin6Line onClick={() => removeFromFavorites(cityWeather.name)} />
+
+              </div>
+            ) : (
+              <p>Failed to fetch weather data for this city.</p>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FavoriteCities;
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import FavoriteCities from '../components/main/FavoriteCities'
 
 function Favorites() {
   return (
-    <div>Favorites</div>
+    <div className="favorites-page">
+      <div className="title">Favorites</div>
+      <div className="favorites">
+        <FavoriteCities/>
+      </div>
+    </div>
   )
 }
 
