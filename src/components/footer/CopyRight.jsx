e8ca67b3f47e2434b075da7d5449d4447903e0a0
Alen Zgurić <zguric.alen@hotmail.com>
2024-04-15 23:00:47 +0200

    Add About page component and import it in App.js; update App.js to include About page in routing

---
+++ src/components/footer/CopyRight.jsx
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom';
+
+const CopyRight = () => {
+
+    const [currentYear, setCurrentYear] = useState(0); 
+  const [copyrightText, setCopyrightText] = useState("");
+
+  useEffect(() => {
+   
+    const getCurrentYear = () => {
+      const date = new Date();
+      return date.getFullYear();
+    };
+
+  
+    setCurrentYear(getCurrentYear());
+
+    const yearDifference =
+      currentYear > 2024 ? `2024 - ${currentYear}` : "2024";
+    setCopyrightText(`| Copyright ® ${yearDifference}. WheatherApp |`);
+  }, [currentYear]);
+
+
+
+  return (
+    <div className='copyright'>
+        <Link to='/about'><p>{copyrightText}</p></Link>
+    </div>
+  )
+}
+
+export default CopyRight
