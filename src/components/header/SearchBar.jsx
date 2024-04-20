d81c144a14bc2acf70697185f3134a8113fb2cd1
Alen Zgurić <zguric.alen@hotmail.com>
2024-04-20 13:51:07 +0200

    add style in SearchBAr component

--- src/components/header/SearchBar.jsx
+++ src/components/header/SearchBar.jsx
@@ -1,12 +1,32 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+import "../../assets/styles/components/header/SearchBar.css";
 
 const SearchBar = () => {
-  const [cityName, setCityName] = useState('');
+  const [cityName, setCityName] = useState("");
   const navigate = useNavigate();
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [isTablet, setIsTablet] = useState(
+    window.innerWidth >= 769 && window.innerWidth <= 1024
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      const windowWidth = window.innerWidth;
+      setIsMobile(windowWidth < 768);
+      setIsTablet(windowWidth >= 769 && windowWidth < 1024);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const handleSearch = () => {
-    if (cityName.trim() !== '') {
+    if (cityName.trim() !== "") {
       // Navigacija na "/myplaces" s unesenim gradom kao parametrom
       navigate(`/citys/${cityName.trim()}`);
 
@@ -15,7 +35,7 @@ const SearchBar = () => {
   };
 
   const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       handleSearch();
 
       setCityName("");
@@ -23,16 +43,17 @@ const SearchBar = () => {
   };
 
   return (
-    <div>
+    <div className="search-bar">
       <input
         type="text"
-        placeholder="Unesite ime grada"
+        placeholder="Search city by name"
         value={cityName}
         onChange={(e) => setCityName(e.target.value)}
         onKeyPress={handleKeyPress}
-        autoComplete='on'
+        autoComplete="on"
       />
-      <button onClick={handleSearch}>Search</button>
+      {!(isMobile || isTablet) &&<button onClick={handleSearch}>Search</button>}
+     {(isMobile || isTablet) && <FaSearch onClick={handleSearch}></FaSearch>}
     </div>
   );
 };
