9779c6b7fd02307960ae93fbd3d313a33970b5fa
Alen Zgurić <zguric.alen@hotmail.com>
2024-04-20 13:54:17 +0200

    Update project files: add cities images, update firebase cache, adjust header and footer, update README
    - Changes to App.css: removed unused App class
    - Changes to App.js: updated header to use nav element,

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,23 +1,50 @@
-import React, { useEffect, useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { FaBars} from "react-icons/fa";
+import { MdLightMode, MdDarkMode } from "react-icons/md";
+
 import logo_dall_e3 from "../../assets/images/background_img/logo_dall_e3.jpg";
 import SearchBar from "../header/SearchBar";
 
-import "../../assets/style/header/NavBar.css";
+import "../../assets/styles/components/header/NavBar.css";
 
 function NavBar() {
-  
   const location = useLocation();
   const showSearchBar = useMemo(() => {
     return location.pathname !== "/about";
   }, [location.pathname]);
 
   const handleSearch = (searchTerm) => {
-    // Ovdje možete postaviti logiku za pretraživanje
     console.log("Search term:", searchTerm);
   };
 
-  useEffect(() => {});
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  // const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  // const [isTablet, setIsTablet] = useState(
+  //   window.innerWidth >= 769 && window.innerWidth <= 1024
+  // );
+
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     const windowWidth = window.innerWidth;
+  //     setIsMobile(windowWidth < 768);
+  //     setIsTablet(windowWidth >= 769 && windowWidth < 1024);
+  //   };
+
+  //   window.addEventListener("resize", handleResize);
+
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, []);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  const toggleDarkMode = ( )=>{
+    setIsDarkMode(!isDarkMode);
+  }
 
   return (
     <div className="navBar">
@@ -30,13 +57,54 @@ function NavBar() {
           </Link>
         </div>
         {showSearchBar && <SearchBar onSearch={handleSearch} />}
+        <div className="menu-icon" onClick={toggleMenu}>
+          <FaBars />
+        </div>
         <div className="navigations">
-          <Link to="/">Home</Link>
-          <Link to="/myplaces">My Places</Link>
-          <Link to="/favorites">Favorites</Link>
-          <Link to="/about">About</Link>
+          <ul>
+            <li>
+            {isDarkMode ? <MdDarkMode onClick={toggleDarkMode}/> : <MdLightMode onClick={toggleDarkMode}/> }
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/myplaces">My Places</Link>
+            </li>
+            <li>
+              <Link to="/favorites">Favorites</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
         </div>
       </div>
+      <div className={`dropdown-content${isMenuOpen ? "open" : ""}`}>
+        <ul>
+          <li>
+            <Link to="/" onClick={toggleMenu}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/myplaces" onClick={toggleMenu}>
+              My Places
+            </Link>
+          </li>
+          <li>
+            <Link to="/favorites" onClick={toggleMenu}>
+              Favorites
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" onClick={toggleMenu}>
+              About
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <hr />
     </div>
   );
 }
