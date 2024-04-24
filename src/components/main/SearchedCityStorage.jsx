75d78873fe309e01106e153cdbef2b7ab2789140
Alen Zgurić <zguric.alen@hotmail.com>
2024-04-24 22:27:16 +0200

    "Update SearchedCityStorage component to allow deletion of cities and reload page; apply styles to MyPlaces page"

--- src/components/main/SearchedCityStorage.jsx
+++ src/components/main/SearchedCityStorage.jsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 const SearchedCityStorage = () => {
-  const storedCities = JSON.parse(localStorage.getItem('selectedCities')) || [];
+  const storedCities = JSON.parse(localStorage.getItem("selectedCities")) || [];
+
+  const handleDeleteCity = (index) => {
+    const updatedCities = [...storedCities];
+    updatedCities.splice(index, 1);
+    localStorage.setItem("selectedCities", JSON.stringify(updatedCities));
+    window.location.reload(); // Ponovno učitavanje stranice kako bi se ažurirao prikaz
+  };
 
   return (
-    <div>
+    <div className="searched-city-storage">
       {storedCities.length === 0 ? (
-        <div className='error-msg'>No recently searched cities found.</div>
+        <div className="error-msg">No recently searched cities found.</div>
       ) : (
         <>
           <h2>Recently Searched Cities</h2>
           <ul>
             {storedCities.map((city, index) => (
               <li key={index}>
-                <div className="city">
-               <Link to={`/citys/${city.name}`}><p>{city.name}</p> </Link>
-                </div>
-                <div className="time">
-                  <p>Searched at:</p>
-                  <span>{city.timestamp}</span>
+                <div className="searched-city-storage-box">
+                  <div className="city">
+                    <Link to={`/citys/${city.name}`}>
+                      <p>{city.name}</p>
+                    </Link>
+                  </div>
+                  <div className="time">
+                    <p>Searched at:</p>
+                    <span>{city.timestamp}</span>
+                  </div>
+                  <div className="delete-btn">
+                    <button onClick={() => handleDeleteCity(index)}>
+                      <RiDeleteBin6Line />
+                    </button>
+                  </div>
                 </div>
               </li>
             ))}
