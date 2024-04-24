75e8fa60afb170f86bf79e99f6f0f9a7267479ae
Alen Zgurić <zguric.alen@hotmail.com>
2024-04-24 21:13:32 +0200

    "Update cityWeatherData to include precipitation description, sunrise, sunset, and visibility"
    The changes include:
    - updating the 'precipitation' field to use the 'description' property from the 'weather'

--- src/utils/cityWeatherData.js
+++ src/utils/cityWeatherData.js
@@ -3,11 +3,14 @@ const cityWeatherData = (city, response) => ({
     feelsLike: response.data.main.feels_like.toFixed(1),
     temp: response.data.main.temp.toFixed(1),
     wind: response.data.wind.speed,
-    precipitation: response.data.weather[0].main,
+    precipitation: response.data.weather[0].description,
     icon: response.data.weather[0].icon,
     minTemp: response.data.main.temp_min.toFixed(1),
     maxTemp: response.data.main.temp_max.toFixed(1),
     windDirection: response.data.wind.deg,
+    sunrise: response.data.sys.sunrise,
+    sunset: response.data.sys.sunset,
+    visibility: response.data.visibility,
   });
   
   export default cityWeatherData;
