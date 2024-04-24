2d65cb678f109743cd488d34d3c74f68cfd6c4cc
Alen Zgurić <zguric.alen@hotmail.com>
2024-04-24 21:31:05 +0200

    Update CityWeatherList.scss and add CopyRight.css; fix typo in CopyRight.jsx

--- src/components/footer/CopyRight.jsx
+++ src/components/footer/CopyRight.jsx
@@ -1,33 +1,33 @@
-import React, {useState, useEffect} from 'react'
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import "../../assets/styles/components/footer/CopyRigh.css";
 
 const CopyRight = () => {
-
-    const [currentYear, setCurrentYear] = useState(0); 
+  const [currentYear, setCurrentYear] = useState(0);
   const [copyrightText, setCopyrightText] = useState("");
 
   useEffect(() => {
-   
     const getCurrentYear = () => {
       const date = new Date();
       return date.getFullYear();
     };
 
-  
     setCurrentYear(getCurrentYear());
 
     const yearDifference =
       currentYear > 2024 ? `2024 - ${currentYear}` : "2024";
-    setCopyrightText(`| Copyright ® ${yearDifference}. WheatherApp |`);
+    setCopyrightText(` Copyright ® ${yearDifference}. WheatherApp `);
   }, [currentYear]);
 
-
-
   return (
-    <div className='copyright'>
-        <Link to='/about'><p>{copyrightText}</p></Link>
+    <div className="copyright">
+      <hr />
+      <Link to="/about">
+        <p>{copyrightText}</p>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default CopyRight
+export default CopyRight;
