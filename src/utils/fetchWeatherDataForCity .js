8d424665a21a9ea1f4a4ca4195ed674ca3017298
Alen Zgurić <zguric.alen@hotmail.com>
2024-05-03 22:37:05 +0200

    Update SearchedCityStorage.css and NavBar.css; adjust styles and add background color to navbar

--- src/utils/fetchWeatherDataForCity .js
+++ src/utils/fetchWeatherDataForCity .js
@@ -4,7 +4,7 @@ import { API_Key } from "../Api/ApiKey";
 export const fetchWeatherDataForCity = async (cityName) => {
   try {
     const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_Key}&units=metric`);
-   // console.log('Weather data for', cityName, ':', response.data);
+   console.log('Weather data for', cityName, ':', response.data);
     return response.data;
   } catch (error) {
     console.error('Error fetching weather data:', error);
